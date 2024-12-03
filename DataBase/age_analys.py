c01be31d9cced4f1a9e448af5b6f2e29782b7f2e
dark_side <161113200+ArzymatK@users.noreply.github.com>
2024-12-03 13:42:42 +0600

    Delete DataBase directory

--- DataBase/age_analys.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-
-print("Enter you date of birth:")
-result = datetime.now()-datetime.strptime(input(">>>"),'%d-%m-%Y')
-print(f"You are {result.days//365}! Have a productive day!")
